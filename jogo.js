5b27e1209df67bce3a6b59de72656e3f561b926e
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2023-05-02 00:38:42 -0300

    nova atualização

--- jogo.js
+++ jogo.js
@@ -1,13 +1,15 @@
 // alert("Ola mundo");
 
-var altura  = 0;
+var altura = 0;
 var largura = 0;
+var vidas = 1;
+var tempo = 30;
 
 // É NESSESARIO DECLARAR A VARIAVÉL FORA DO ESCOPO DA FUNCAO PARA PODE USAR ELA DENTRO DA FUNCAO
 
-function ajustaTamanhoPalcoJogo(){
+function ajustaTamanhoPalcoJogo() {
 
-    altura  = window.innerHeight;   // METODO PARA VER A DIMENSAO DA JANELA
+    altura = window.innerHeight; // METODO PARA VER A DIMENSAO DA JANELA
     largura = window.innerWidth;
 
     console.log(largura, altura);
@@ -15,16 +17,40 @@ function ajustaTamanhoPalcoJogo(){
 
 ajustaTamanhoPalcoJogo();
 
-function posicaoRandomica (){
+var cronometro = setInterval(function() {
+    tempo -= 1;
+
+    if (tempo < 0) {
+        clearInterval(cronometro) // Eliminando a funcao da memoria com clearInterval
+        clearInterval(criaMosquito)
+        window.location.href = "vitoria.html"
+    } else {
+        document.getElementById("cronometro").innerHTML = tempo;
+    }
+
+}, 1000)
+
+function posicaoRandomica() {
 
     // remover o mosquito anterior (caso exista)
 
-    if(document.getElementById("mosquito")){
+    if (document.getElementById("mosquito")) {
         document.getElementById("mosquito").remove()
+
+        // console.log("O Elemento selecionado foi: v" + vidas);
+        if (vidas > 3) {
+
+            // alert("interromper o jogo (Gamer Over!)")
+            window.location.href = "fim_de_jogo.html"
+        } else {
+
+            document.getElementById("v" + vidas).src = "imagens/coracao_vazio.png";
+            vidas++
+        }
     }
 
-    var posicaoX = Math.floor(Math.random() * largura) - 90    // METODO Math.random() PARA TRAZER UM NUMERO RANDOMICO
-    var posicaoY = Math.floor(Math.random() * altura)  - 90    // METODO Math.floor()  PARA ARREDONDAR AS CASAS DECIMAIS PARA BAIXO
+    var posicaoX = Math.floor(Math.random() * largura) - 90 // METODO Math.random() PARA TRAZER UM NUMERO RANDOMICO
+    var posicaoY = Math.floor(Math.random() * altura) - 90 // METODO Math.floor()  PARA ARREDONDAR AS CASAS DECIMAIS PARA BAIXO
 
     posicaoX = posicaoX < 0 ? 0 : posicaoX
     posicaoY = posicaoY < 0 ? 0 : posicaoY
@@ -35,23 +61,27 @@ function posicaoRandomica (){
     // CRIAR O ELEMENTO HTML
     // document.createElement("img")
 
-    var mosquito = document.createElement("img")          // cliando o elemento e passando para a variavel
-    mosquito.src="imagens/mosquito.png"                   // incluindo o novo elemtno no body da pagina
-    mosquito.className   = tamanhoAleatorio() + ' ' + ladoAleatorio();
-    mosquito.style.left  = posicaoX + "px"
-    mosquito.style.top   = posicaoY + "px"
+    var mosquito = document.createElement("img") // cliando o elemento e passando para a variavel
+    mosquito.src = "imagens/mosquito.png" // incluindo o novo elemtno no body da pagina
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio();
+    mosquito.style.left = posicaoX + "px"
+    mosquito.style.top = posicaoY + "px"
     mosquito.style.position = "absolute"
     mosquito.id = "mosquito"
+
+    mosquito.onclick = function() {
+        this.remove();
+    }
 
     document.body.appendChild(mosquito);
 
 }
 
-function tamanhoAleatorio(){
+function tamanhoAleatorio() {
     var classe = Math.floor(Math.random() * 3);
     // console.log('valor -> ' + classe);
 
-    switch (classe){
+    switch (classe) {
         case 0:
             return "mosquito1"
 
@@ -64,12 +94,12 @@ function tamanhoAleatorio(){
     }
 }
 
-function ladoAleatorio(){
+function ladoAleatorio() {
 
     var classe = Math.floor(Math.random() * 2);
     // console.log('valor -> ' + classe);
 
-    switch (classe){
+    switch (classe) {
         case 0:
             return "ladoA"
 
